083724e5cc8afa1979412e4461edb06cf42d1610
GeovanniGblackzx <166544426+GeovanniGblackzx@users.noreply.github.com>
2024-04-11 16:50:01 -0300

    triâgulos.py

--- triâgulos.py
+++ triâgulos.py
@@ -2,8 +2,8 @@ lado1 = float(input("Digite o primeiro lado "))
 lado2 = float(input("Digite o segundo lado "))
 lado3 = float(input("Digite o terceiro lado "))
 if lado1 == lado2 and lado2 == lado3 and lado1 == lado3:
-    print ("Triângulo Equilátero")
+    print ("É possível formar um triângulo equilátero")
 elif lado1 != lado2 and lado2 != lado3 and lado1 != lado3:
-    print ("Triângulo Escaleno")
+    print ("É possível formar um triângulo escaleno")
 else:
-    print ("Triângulo Isósceles")
+    print ("É possível formar um triângulo isósceles")
