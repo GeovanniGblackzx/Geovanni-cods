d31aea4d57704d4fcaf1d609181627489040de6f
GeovanniGblackzx <166544426+GeovanniGblackzx@users.noreply.github.com>
2024-04-25 17:06:42 -0300

    Add files via upload

---
+++ juros.py
@@ -0,0 +1,5 @@
+valor = float(input("Qual o valor? R$"))
+juros = float(input("Qual a taxa de juros? "))
+tempo = float(input("Qual o tempo acumulado? "))
+total = valor * juros * tempo
+print (total)
